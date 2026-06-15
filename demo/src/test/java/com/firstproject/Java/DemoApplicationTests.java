b6def65b783412a2dca8da50198d2079c34dd2be
Yash Maulekhi <yashmaulekhi63@gmail.com>
2026-06-15 19:04:43 +0530

    Add files via upload

---
+++ demo/src/test/java/com/firstproject/Java/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstproject.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
